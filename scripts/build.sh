77bfa034c82b760a1923d34fba398076765f97cf
JarredTD <jarred.desrosiers@yahoo.com>
2024-04-11 03:48:46 -0400

    fix(build): Update linker path in .cargo/config

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ targets=(
     "aarch64-apple-darwin"   # Mac M1 (ARM64)
     "x86_64-apple-darwin"    # Mac Intel (x86_64)
     "x86_64-pc-windows-gnu"  # Windows (GNU)
-    "x86_64-unknown-linux-gnu" # Linux (x86_64)
+    "x86_64-unknown-linux-gnu-local" # Linux (x86_64)
 )
 
 for target in "${targets[@]}"
