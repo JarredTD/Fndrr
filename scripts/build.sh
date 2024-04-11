636b414aacfa69bed2c3f16b3a126d67bac4fb94
JarredTD <jarred.desrosiers@yahoo.com>
2024-04-11 03:41:54 -0400

    build: Add build script, linkers for other platforms, & CI workflow

---
+++ scripts/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rm -rf target/
+
+targets=(
+    "aarch64-apple-darwin"   # Mac M1 (ARM64)
+    "x86_64-apple-darwin"    # Mac Intel (x86_64)
+    "x86_64-pc-windows-gnu"  # Windows (GNU)
+    "x86_64-unknown-linux-gnu" # Linux (x86_64)
+)
+
+for target in "${targets[@]}"
+do
+    echo "Building for $target..."
+    cargo build --target $target --release
+    if [ $? -eq 0 ]; then
+        echo "Build for $target completed successfully."
+    else
+        echo "Build for $target failed."
+        continue
+    fi
+done
